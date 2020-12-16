61b9a93fe19e50870c05164cde533b44299ffc6a
antonio-fiorentino <antoniofiorentino16@gmail.com>
2020-12-16 17:34:19 +0100

    Ordine sbagliato

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 var listaOutput = document.getElementById('lista-cognomi');
 var cognome = prompt("Qual'è il suo Cognome");
-var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Verdi', 'Balsano', cognome];
+var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Verdi', 'Balsano'];
+var numero = document.getElementById('numero');
 
 var contenutoPrecedente;
-
+listaCognomi.push(cognome);
 for (var i = 0; i < listaCognomi.length; i++) {
 
   contenutoPrecedente = listaOutput.innerHTML;
   listaOutput.innerHTML = contenutoPrecedente + '<li>' + listaCognomi[i] + '</li>';
+  listaCognomi.sort();
+  var ordine = listaCognomi.indexOf(cognome)
 }
-listaCognomi.sort();
+numero.innerText = (ordine + 1);
